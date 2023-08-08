dc2d651052375dc502b7d077934d42047147ede7
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-08 12:25:45 +0000

    Check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,19 @@
 #include "lists.h"
 
 /**
+* gateway - Checks if we are still eligible to enter
+*
+*@t1: Var one
+*@t2: Var two
+*@t3: Var three
+*
+*Return: True or False
+*/
+int gateway(listint_t *t1, listint_t *t2, listint_t *t3)
+{
+	return (t1 && t2 && t3->next);
+}
+/**
 * check_cycle - Checks if a singly linked list contains a cycle
 *
 *@list: The singly linked list
@@ -8,15 +21,15 @@
 */
 int check_cycle(listint_t *list)
 {
-	listint_t *tmp, *tmp1;
-	tmp = tmp1 = list;
+	listint_t *tmp1, *tmp2;
 
-	while (tmp)
+	tmp1 = tmp2 = list;
+	while (gateway(tmp1, tmp2, tmp2->next))
 	{
-		if (tmp->next == list || tmp1 == tmp)
+		tmp1 = tmp1->next;
+		tmp2 = tmp2->next->next;
+		if (tmp1 == tmp2)
 			return (1);
-		tmp1 = tmp;
-		tmp = tmp->next;
 	}
 	return (0);
 }
