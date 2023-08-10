16b34cf3a75aacf248faefd667abc9a9eca56bf6
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-10 01:24:00 +0000

    Remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+# 1/usr/bin/python3
+
+
+def remove_char_at(str, n):
+    new_str = ""
+    for i in range(len(str)):
+        if n == i:
+            new_str += " "
+        else:
+            new_str += str[i]
+    return new_str
