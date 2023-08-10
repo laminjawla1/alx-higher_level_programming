bbb1fef3804d552649e76aeea5ef49f6a3c1e6f3
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-10 16:21:32 +0000

    Reformat

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
 
+
+def main():
     a = 1
     b = 2
+
     print("{} + {} = {}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    main()
