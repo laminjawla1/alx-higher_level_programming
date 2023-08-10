3cc25757fb464fa19d97661b22e7737bc47cad47
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-10 17:53:31 +0000

    Own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
 
 from sys import argv, exit
-from calculator_1 import (add, sub, mul, div)
+from calculator_1 import add, sub, mul, div
 
 
 def main():
-	if len(argv) != 4:
-		print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-		exit(1)
-	calculate()
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    calculate(argv[1], argv[2], argv[3])
+
+
+def calculate(n1, op, n2):
+    n1 = int(n1)
+    n2 = int(n2)
+    match (op):
+        case "+":
+            print("{} + {} = {}".format(n1, n2, add(n1, n2)))
+        case "-":
+            print("{} - {} = {}".format(n1, n2, sub(n1, n2)))
+        case "*":
+            print("{} * {} = {}".format(n1, n2, mul(n1, n2)))
+        case "/":
+            print("{} / {} = {}".format(n1, n2, div(n1, n2)))
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+
+
 if __name__ == "__main__":
-	main()
+    main()
