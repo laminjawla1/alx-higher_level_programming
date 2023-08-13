4e7bb2b1af971c59b7d9ce8e4b5229e0055feb80
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-13 06:31:52 +0000

    New list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
+
 def new_in_list(my_list, idx, element):
-    if isinstance(my_list, list):
-        if 0 <= idx < len(my_list):
-            new_list = [n for n in my_list]
-            new_list[idx] = element
-    return my_list
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+
+    new_list = [x for x in my_list]
+    new_list[idx] = element
+    return new_list
