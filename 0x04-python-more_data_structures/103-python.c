3f759e17858e914f6f4082e42492f5ab745f0ad4
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-16 23:12:05 +0000

    Python

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -8,17 +8,32 @@
 */
 void print_python_bytes(PyObject *p)
 {
+	char *trying_string;
+	unsigned long int threshold, i;
+	size_t list_size;
+
 	PyListObject *plo = (PyListObject *)p;
 	PyBytesObject *pbo = (PyBytesObject *)p;
 
+	list_size = plo->ob_base.ob_size;
+	trying_string = pbo->ob_sval;
 	printf("[.] bytes object info\n");
+
 	if (!PyBytes_Check(p))
 	{
 		printf("    [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-	printf("    size: %ld\n", plo->ob_base.ob_size);
-	printf("    trying string: %s\n", pbo->ob_sval);
+
+	printf("    size: %ld\n", list_size);
+	printf("    trying string: %s\n", trying_string);
+	threshold = (list_size >= 10) ? 10 : list_size + 1;
+
+	printf("first %ld bytes: ", threshold);
+	for (i = 0; i < threshold; i++)
+		(trying_string[i] >= 0) ? printf(" %02x", trying_string[i]) :
+			printf(" %02x", 256 + trying_string[i]);
+	printf("\n");
 }
 /**
 * print_python_list - Prints information about python list
