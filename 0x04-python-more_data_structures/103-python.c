2187f751cb9dbf2ee57b9d461198ceca47c94a52
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-16 20:13:01 +0000

    List info and bytes object

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,16 +1,6 @@
 #include <Python.h>
 
 /**
-* print_python_list - Prints information about python list
-*
-*@p: Python object
-*
-*/
-void print_python_list(PyObject *p)
-{
-
-}
-/**
 * print_python_bytes - Prints bytes bytes objects
 *
 *@p: Python object
@@ -18,5 +8,39 @@ void print_python_list(PyObject *p)
 */
 void print_python_bytes(PyObject *p)
 {
+	PyListObject *plo = (PyListObject *)p;
+	PyBytesObject *pbo = (PyBytesObject *)p;
 
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("    [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	printf("    size: %ld\n", plo->ob_base.ob_size);
+	printf("    trying string: %s\n", pbo->ob_sval);
+}
+/**
+* print_python_list - Prints information about python list
+*
+*@p: Python object
+*
+*/
+void print_python_list(PyObject *p)
+{
+	int i;
+	PyListObject *pp = (PyListObject *)p;
+	PyObject *item;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the python list %ld\n", pp->ob_base.ob_size);
+	printf("[*] Allocated = %ld\n", pp->allocated);
+
+	for (i = 0; i < pp->ob_base.ob_size; i++)
+	{
+		item = pp->ob_item[i];
+		printf("Element %d: %s\n", i, item->ob_type->tp_name);
+		if (PyBytes_Check(item))
+			print_python_bytes(item);
+	}
 }
