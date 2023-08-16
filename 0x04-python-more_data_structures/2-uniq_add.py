85d77bf7b84ac2b7b20b9eaecb207beeffec68f2
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-16 11:16:16 +0000

    Unique add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     result = 0
+    history = []
     for n in my_list:
-        if my_list.count(n) == 1:
+        if n not in history:
             result += n
+            history.append(n)
     return result
