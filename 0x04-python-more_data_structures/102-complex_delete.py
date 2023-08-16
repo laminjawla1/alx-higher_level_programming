6169a9da01c543e5be7d7ad7959c92a8830e5016
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-16 17:50:59 +0000

    co

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     keys = a_dictionary.keys()
-    for key in keys:
+    for key in list(keys):
         if a_dictionary.get(key) == value:
             del a_dictionary[key]
     return a_dictionary
