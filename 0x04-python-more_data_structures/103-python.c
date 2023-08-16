ae6bb7c3ea02ffb7918efb22bb9ae79f9411d900
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-16 18:19:22 +0000

    Template for Cpython

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+
+/**
+* print_python_list - Prints information about python list
+*
+*@p: Python object
+*
+*/
+void print_python_list(PyObject *p)
+{
+
+}
+/**
+* print_python_bytes - Prints bytes bytes objects
+*
+*@p: Python object
+*
+*/
+void print_python_bytes(PyObject *p)
+{
+
+}
