0dbbe30053efccd234205a3c70751c8c3b61882d
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-16 15:56:12 +0000

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from functools import reduce
+
+
+def get_numerator(l):
+    n = 0
+    for t in l:
+        n += reduce(lambda x, y: x * y, t)
+    return n
+
+
+def get_denominator(l):
+    n = 0
+    for t in l:
+        n += t[1]
+    return n
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    numerator = get_numerator(my_list)
+    denominator = get_denominator(my_list)
+    return numerator / denominator
