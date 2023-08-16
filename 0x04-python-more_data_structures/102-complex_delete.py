55b8bce82e3f71b7b94a81679e2588bdf99eadf7
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-16 17:45:44 +0000

    Complex delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    trashes = list()
-    for d in a_dictionary:
-        try:
-            trashes = [d for d in a_dictionary if a_dictionary[d] == value]
-        except KeyError:
-            pass
-    for trash in trashes:
-        del a_dictionary[trash]
+    keys = a_dictionary.keys()
+    for key in keys:
+        if a_dictionary.get(key) == value:
+            del a_dictionary[trash]
     return a_dictionary
