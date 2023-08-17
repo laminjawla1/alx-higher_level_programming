cd4050614204cd223078db3332f0020f476e6a1a
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-17 18:10:49 +0000

    PyListObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -36,16 +36,17 @@ void print_python_bytes(PyObject *p)
 void print_python_list(PyObject *p)
 {
 	int i;
-	PyListObject *pp = (PyListObject *)p;
+	PyVarObject *pvo = (PyVarObject *)p;
+	PyListObject *plo = (PyListObject *)p;
 	PyObject *item;
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of the python list = %ld\n", pp->ob_base.ob_size);
-	printf("[*] Allocated = %ld\n", pp->allocated);
+	printf("[*] Size of the python list = %ld\n", pvo->ob_size);
+	printf("[*] Allocated = %ld\n", plo->allocated);
 
-	for (i = 0; i < pp->ob_base.ob_size; i++)
+	for (i = 0; i < pvo->ob_size; i++)
 	{
-		item = pp->ob_item[i];
+		item = plo->ob_item[i];
 		printf("Element %d: %s\n", i, item->ob_type->tp_name);
 		if (PyBytes_Check(item))
 			print_python_bytes(item);
