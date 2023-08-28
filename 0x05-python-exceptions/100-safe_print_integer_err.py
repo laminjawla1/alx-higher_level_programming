7c74e0d47f0c7f4baac968081e28c0d979390a58
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-28 12:02:20 +0000

    Raise Exception

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        sys.stderr.write("Exception: {}".format(e))
+        return False
