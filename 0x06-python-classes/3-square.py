08f614912beba0c7c5c8c674aef198efe0d25022
Lamin Jawla <laminjawla1001@gmail.com>
2023-08-29 12:13:32 +0000

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Module demonstrating private instance variables
+"""
+
+
+class Square:
+    """
+    Square - The blueprint
+    """
+
+    def __init__(self, size=0):
+        """
+        __init__ - The initializer
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size: int = size
+
+    def area(self):
+        """
+        area - Calculates the area of a circle
+        """
+        return self.__size * self.__size
