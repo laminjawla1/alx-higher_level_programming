efa7b153bba5743cdf29a9ca2560ed9273dfaac2
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-11 16:29:32 +0000

    Integer validator

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+"""
+The rectangle module
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    The Rectangle class inheriting from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializer
+        """
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+
+        self.__width = width
+        self.__height = height
