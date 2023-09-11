280118fe28fb0030e48dd8b1b6a687203ebf947e
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-11 11:28:37 +0000

    is same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Implements the is_same_class function"""
+
+
 def is_same_class(obj, a_class):
     """Return true of obj is an instance of a_class else false"""
-    return isinstance(obj, a_class)
+    return type(obj) == a_class
