23ff83fdc73f45845624da79aa399229e1ecfae2
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-12 12:47:54 +0000

    Student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,7 +6,7 @@ class Student:
     """Blueprint for the student objects"""
 
     def __init__(self, first_name, last_name, age):
-        self.first_name - first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
