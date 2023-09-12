f162dd46596a1caa95df095a607d43af4b91c293
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-12 13:00:53 +0000

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""The student module"""
+
+
+class Student:
+    """Blueprint for the student objects"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            result = {}
+            for key, val in self.__dict__.items():
+                if key in attrs:
+                    result.update({key: val})
+            return result
+        return self.__dict__
