59ccab545ee2f04df700d9d3c1ae033fcd859e57
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-12 10:34:55 +0000

    Save to json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Save to json file module"""
 
+import json
+
 
 def save_to_json_file(my_obj, filename):
     """Save to json file"""
-    with open(filename, "wb", encoding="utf-8") as f:
-        f.write(my_obj)
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
