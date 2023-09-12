09307696c79e01709755d179476a0872c725c92d
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-12 12:33:17 +0000

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,15 +4,4 @@
 
 def class_to_json(obj):
     """Class to json"""
-    result = {}
-
-    for attr, value in vars(obj).items():
-        if type(value) in [str, int, bool]:
-            result.update({attr: value})
-        elif type(value) == list:
-            result_dict[attr] = [class_to_json(item) for item in value]
-        elif type(value) == dict:
-            result_dict[attr] = {
-                key: class_to_json(value) for key, value in attr_value.items()
-            }
-    return result
+    return obj.__dict__
