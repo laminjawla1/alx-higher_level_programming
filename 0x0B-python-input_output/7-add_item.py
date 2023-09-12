d9fed8257e0bc9d7ce8251118214724e5f7287e6
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-12 12:01:48 +0000

    Add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,13 +10,11 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 def main():
     """Entry point to the program"""
     filename = "add_item.json"
-    my_list = []
     try:
         my_list = load_from_json_file(filename)
-    except json.decoder.JSONDecodeError:
-        pass
-    for arg in argv[1:]:
-        my_list.append(arg)
+    except:
+        my_list = []
+    my_list.extend(argv[1:])
     save_to_json_file(my_list, filename)
 
 
