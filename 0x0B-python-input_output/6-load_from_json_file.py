31fa2e5d330d59d3ef391bf94c3dc9786ded6c58
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-12 10:57:55 +0000

    load from json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Load from json file module"""
 
+import json
+
 
 def load_from_json_file(filename):
     """Loads a json obj from a file"""
     with open(filename, "r") as f:
-        return json.load(f)
+        return json.dumps(json.load(f))
