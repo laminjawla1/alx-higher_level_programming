d6af8b4daee22c2ea51595e7d0e5b50867892b4f
Lamin Jawla <laminjawla1001@gmail.com>
2023-09-12 12:46:43 +0000

    Student

--- 0x0B-python-input_output/main.py
+++ 0x0B-python-input_output/main.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" My class module
-"""
+Student = __import__('9-student').Student
 
-class MyClass:
-    """ My class
-    """
+students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
 
-    def __init__(self, name):
-        self.name = name
-        self.number = 0
-
-    def __str__(self):
-        return "[MyClass] {} - {:d}".format(self.name, self.number)
+for student in students:
+    j_student = student.to_json()
+    print(type(j_student))
+    print(j_student['first_name'])
+    print(type(j_student['first_name']))
+    print(j_student['age'])
+    print(type(j_student['age']))
