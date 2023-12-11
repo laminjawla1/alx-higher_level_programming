56686f0260cffc3c8ec6c21ce0a6a9f600cd4fd9
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-11 14:44:56 +0000

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,22 +1,16 @@
 #!/usr/bin/node
 function main () {
-  const numbers = getNumbers(process.argv);
+  const numbers = process.argv.slice(2).map(Number);
 
   if (numbers.length < 2) {
     console.log(0);
     return;
   }
 
-  numbers.sort();
-  console.log(numbers[numbers.length - 2]);
-}
-function getNumbers (list) {
-  const numbers = [];
-  const len = list.length;
+  numbers.sort((x, y) => {
+    return y - x;
+  });
 
-  for (let i = 2; i < len; i++) {
-    numbers.push(parseInt(list[i]));
-  }
-  return numbers;
+  console.log(numbers[1]);
 }
 main();
