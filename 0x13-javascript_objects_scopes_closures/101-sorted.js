aa246a7791722d5c182afb7e4d6e0a41c43c5d8a
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-12 16:25:10 +0000

    Sorted occurrences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,29 +1,16 @@
 #!/usr/bin/node
-function main () {
-  const dict = require('./101-data.js').dict;
+const dict = require('./101-data.js').dict;
 
-  const newKeys = getUniqueKeys(Object.values(dict));
-  const newDict = init(newKeys);
+const newKeys = [...new Set(Object.values(dict))];
+const newDict = init(newKeys);
 
-  for (const d in dict) {
-    newDict[dict[d]].push(d);
-  }
-  console.log(newDict);
+for (const d in dict) {
+  newDict[dict[d]].push(d);
 }
-function getUniqueKeys (list) {
-  const newList = [];
-
-  for (let i = 0; i < list.length; i++) {
-    if (newList.indexOf(list[i]) === -1) {
-      newList.push(list[i]);
-    }
-  }
-  return newList;
-}
+console.log(newDict);
 function init (keys) {
   const dict = {};
 
   for (let i = 0; i < keys.length; i++) { dict[keys[i]] = []; }
   return dict;
 }
-main();
