b5d2f40cda24ba20fa9b43fa115ff5f4c342c4ff
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-14 16:33:37 +0000

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This file implements a program
+that query the database htbn_0e_usa
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost", user=user, passwd=password, db=database, port=3306
+    )
+
+    cur = db.cursor()
+
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities INNER JOIN states ON states.id=cities.state_id""")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
