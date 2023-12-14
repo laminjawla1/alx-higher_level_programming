130acdd434dcdeba54fd0da3e403855b34e5565f
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-14 20:51:17 +0000

    First state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This file implements the base model
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (Column, Integer, String, MetaData)
+
+
+_metadata = MetaData()
+Base = declarative_base(metadata=_metadata)
+
+
+class State(Base):
+    """
+    The state model - A blue print for State objects
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
