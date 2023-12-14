1b93cda47b29c2bd1c294891c9a02dc9bc03866e
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-14 11:41:32 +0000

    get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This file implements a program
+that query the database htbn_0e_usa
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost", user=user, passwd=password, db=database, port=3306
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,3 @@
+# Object Relational Mapping (ORM)
+
+Object–relational mapping (ORM, O/RM, and O/R mapping tool) in computer science is a programming technique for converting data between a relational database and the heap of an object-oriented programming language. This creates, in effect, a virtual object database that can be used from within the programming language.
