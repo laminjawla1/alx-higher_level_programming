5bb73c821cacb1292436e86654ac2840e0b9fe40
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-15 12:13:25 +0000

    Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This file implements a program
+that query the database htbn_0e_usa
+"""
+import sys
+import MySQLdb
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".
+        format(user, password, database)
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.get_one(State, 2)
+    state.name = "New Mexico"
+    session.commit()
+
+
+if __name__ == "__main__":
+    main()
