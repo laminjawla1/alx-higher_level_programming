6019ca02bc689b23f91920a08fa6483779c3638d
Lamin Jawla <laminjawla1001@gmail.com>
2023-12-15 12:23:57 +0000

    Delete states

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This file implements a program
+that query the database htbn_0e_usa
+"""
+import sys
+import MySQLdb
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".
+        format(user, password, database)
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%'))
+    for state in states:
+        session.delete(state)
+    session.commit()
+
+
+if __name__ == "__main__":
+    main()
