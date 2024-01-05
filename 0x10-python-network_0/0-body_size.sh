ff458b6d26d6166c6c387b29da58c126a01214cc
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-05 10:19:08 +0000

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-response=$(curl -sI $1)
-echo "$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')"
+response=$(curl -sI "$1")
+echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r'
