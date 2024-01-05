7e4293cbbcd15f92fbb93bb898c2112e204f7120
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-05 17:34:59 +0000

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,15 +2,13 @@
 """This script implements a function that finds the peak in a list"""
 def find_peak(list_of_integers):
     """Finds the peak index in an array"""
-    if len(list_of_integers) < 3:
-        return None
-    start = 0
-    end = len(list_of_integers) - 1
-
-    while start < end:
-        middle = start + (end - start) / 2
-        if list_of_integers[middle] > list_of_integers[middle + 1]:
-            end = middle
-        else:
-            start = middle + 1
-    return start
+    if len(list_of_integers) > 2:
+        start = 0
+        end = len(list_of_integers) - 1
+        while start < end:
+            middle = start + (end - start) // 2
+            if list_of_integers[middle] > list_of_integers[middle + 1]:
+                end = middle
+            else:
+                start = middle + 1
+        return start
