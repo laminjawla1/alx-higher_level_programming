8c4a49927a59ddf27186e90e19fef84677d2bb8d
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-05 12:26:27 +0000

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL as an argument, sends a GET request to the URL and displays the body of the response
-curl -sdG "X-School-User-Id=98" "$1" -X GET
+curl -sd "X-School-User-Id=98" "$1" -X GET
