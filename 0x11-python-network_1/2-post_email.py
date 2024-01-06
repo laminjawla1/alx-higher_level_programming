36ac082825d01c1af8fce35502ab8684e2d62be5
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-06 13:41:22 +0000

    POST

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,10 +11,11 @@ import urllib.request
 
 def main():
     data = urllib.parse.urlencode({"email": sys.argv[2]})
-    data = data.encode('ascii')
+    data = data.encode("ascii")
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
-        print(response.read())
+        print(response.read().decode("utf-8"))
+
 
 if __name__ == "__main__":
     main()
