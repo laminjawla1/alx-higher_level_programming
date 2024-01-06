4c25e2b9b77a7d0e0c756bfeb0abcaf915cce8c3
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-06 12:48:43 +0000

    Response header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-"""This script fetches data from
-    https://alx-intranet.hbtn.io/status
 """
-
+Implemments a python script to get the status of a route
+"""
 
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+def main():
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    main()
