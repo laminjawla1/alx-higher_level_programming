79d9e3695a6c975ab69fb4f7bf38f7fe44e03267
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-06 09:25:54 -0800

    My GitHub

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes your GitHub
+credentials (username and password) and uses
+the GitHub API to display your id
+"""
+import sys
+import requests
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = "https://api.github.com/users/{}".format(username)
+    response = requests.get(url, auth=(username, password))
+    result = response.json()
+    print(result.get("id"))
+
+
+if __name__ == "__main__":
+    main()
