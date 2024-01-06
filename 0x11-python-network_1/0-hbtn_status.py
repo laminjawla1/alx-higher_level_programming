7a32cd9bb9facaf329cdae5ae5041804ae9a45c3
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-06 12:15:42 +0000

    What's my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,16 +5,11 @@ Implemments a python script to get the status of a route
 import urllib.request
 
 
-def main():
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    body = response.read()
 
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
-
-if __name__ == "__main__":
-    main()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
