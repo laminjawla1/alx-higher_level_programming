ccb4666b278dab909ad03c15fa9bc9625967dfa2
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-06 12:35:53 +0000

    Response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
+Write a Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response.
 """
+import sys
+import urllib.request
+
+
+def main():
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get("X-Request-Id"))
+
+
+if __name__ == "__main__":
+    main()
