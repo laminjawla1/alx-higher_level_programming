efbb34f7250af2b5307e0514ed5616ddc5e93f35
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-06 13:51:00 +0000

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, sends a request to the
+URL and displays the body of the response (decoded in utf-8).
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+def main():
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.URLError as e:
+        print("Error code: {}".format(e.code))
+
+
+if __name__ == "__main__":
+    main()
