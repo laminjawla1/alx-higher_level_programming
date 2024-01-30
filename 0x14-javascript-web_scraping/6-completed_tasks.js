0e6de71fa6cfe2974680c020804ad6e59a47b36a
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-30 07:54:04 -0800

    How many completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,7 +9,7 @@ const url = process.argv[2];
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
+  } else if (response.statusCode === 200) {
     const todos = JSON.parse(body);
     const result = {};
     todos.forEach(todo => {
@@ -21,5 +21,7 @@ request.get(url, (error, response, body) => {
       }
     });
     console.log(result);
+  } else {
+    console.log(`code: ${response.statusCode}`);
   }
 });
