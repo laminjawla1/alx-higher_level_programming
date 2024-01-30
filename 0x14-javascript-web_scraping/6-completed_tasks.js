fd5283e8370e6f476368cfb936af23c7a039d5ce
Lamin Jawla <laminjawla1001@gmail.com>
2024-01-30 07:39:10 -0800

    How many completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/**
+ * Computes the number of tasks completed by user id..
+ */
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const todos = JSON.parse(body);
+    const result = {}
+    todos.forEach(todo => {
+      if (todo.completed) {
+        let initialResult = result[todo.userId];
+	if (initialResult === undefined)
+	  result[todo.userId] = 1;
+	else
+	  result[todo.userId] = result[todo.userId] + 1;
+      }
+    });
+    console.log(result);
+  }
+});
